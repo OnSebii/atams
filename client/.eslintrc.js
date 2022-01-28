431aaa60474b29be7cb2d894db477126d81a465f
OnSebii <lang.sebastian.006@gmail.com>
2022-01-28 08:03:04 +0100

    design update

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -11,5 +11,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'linebreak-style': 0,
+    'max-len': ['error', { code: 180 }],
   },
 };
--- client/src/App.vue
+++ client/src/App.vue
@@ -2,9 +2,7 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-        <v-img alt="Vuetify Logo" class="shrink mr-2" contain src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png" transition="scale-transition" width="40" />
-
-        <v-img alt="Vuetify Name" class="shrink mt-1 hidden-sm-and-down" contain min-width="100" src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png" width="100" />
+        <v-img alt="atams name" class="shrink mt-1 hidden-sm-and-down" contain min-width="100" src="@/assets/logo.png" width="100" />
       </div>
 
       <v-spacer></v-spacer>
@@ -13,6 +11,25 @@
     <v-main>
       <router-view />
     </v-main>
+    <v-bottom-navigation>
+      <v-btn value="recent">
+        <span>Recent</span>
+
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+
+      <v-btn value="favorites">
+        <span>Favorites</span>
+
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn value="nearby">
+        <span>Nearby</span>
+
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
   </v-app>
 </template>
 
