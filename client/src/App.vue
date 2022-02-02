63e3f03232eeea848867fecb53e1edb3cefa2ff1
OnSebii <lang.sebastian.006@gmail.com>
2022-02-02 20:10:32 +0100

    client update

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,32 +4,23 @@
       <div class="d-flex align-center">
         <v-img alt="atams name" class="shrink mt-1 hidden-sm-and-down" contain min-width="100" src="@/assets/logo.png" width="100" />
       </div>
-
       <v-spacer></v-spacer>
+      <v-btn class="ma-2" outlined color="#A3C3D9" to="/home"> Home </v-btn>
+      <v-btn class="ma-2" outlined color="#A3C3D9"> Outlined Button </v-btn>
+      <v-btn class="ma-2" outlined color="#A3C3D9" to="/trips"> Trips </v-btn>
     </v-app-bar>
 
     <v-main>
       <router-view />
     </v-main>
-    <v-bottom-navigation>
-      <v-btn value="recent">
-        <span>Recent</span>
 
-        <v-icon>mdi-history</v-icon>
-      </v-btn>
-
-      <v-btn value="favorites">
-        <span>Favorites</span>
-
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn value="nearby">
-        <span>Nearby</span>
-
-        <v-icon>mdi-map-marker</v-icon>
-      </v-btn>
-    </v-bottom-navigation>
+    <v-footer color="#E9ECF5" padless>
+      <v-row justify="center" color="A3C3D9" no-gutters>
+        <v-col col="4"></v-col>
+        <v-col class="py-4 text-center" col="4"> baily.at | <strong>atams</strong></v-col>
+        <v-col class="py-4 text-right" col="4"> <a to="/about" class="pr-4">Impressum</a></v-col>
+      </v-row>
+    </v-footer>
   </v-app>
 </template>
 
@@ -38,7 +29,6 @@ export default {
   name: 'App',
 
   data: () => ({
-    updateAvailable: null,
     offline: null,
   }),
   created() {
---
+++ client/src/views/Trips.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Trips</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Trips',
+
+  data: () => ({}),
+  created() {},
+  methods: {},
+};
+</script>
